4400c49891d5ae4323895277153e4d0f1d00c910
AyushiHothi <ayushihothi@gmail.com>
2024-05-30 15:32:11 +0530

    Portfolio

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -12,7 +12,7 @@ const Hero = () => {
                 <p className={styles.description}>I was born and raised in Surat, and in 2024 I graduated with a Bachelor of Computer Application (BCA) from Veer Narmad South Gujarat University. During my time in college, I solidified my understanding of front-end development, backend development and web design principles. I always try to learn something new and stay current with advancements in business and technology.</p>
                 <a href="mailto: ayushihothi@gmail.com" className={styles.contactBtn}>Contact Me</a>
             </div>
-            <img src={getImageUrl("hero/hero.png")} alt='Hero Image of Me' className={styles.heroImg} />
+            <img src={getImageUrl("hero/heroImage.png")} alt='Hero Image of Me' className={styles.heroImg} />
             <div className={styles.topBlur} />
             <div className={styles.bottomBlur} />
         </section>
